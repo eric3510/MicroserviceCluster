1e17eb6555cba99c8844fb989ca61f22dd2282ee
wangqiang <eric351035@gmail.com>
2018-06-28 17:08:09 +0800

    初始化项目

---
+++ server1/src/main/java/Application.java
@@ -0,0 +1,16 @@
+import org.springframework.boot.container.core.service.impl.BaseServiceImpl;
+import org.springframework.web.bind.annotation.RestController;
+
+/***
+ * @author 王强 Email : 
+ * @version 创建时间：2018/6/28
+ * Application
+ */
+@RestController
+public class Application extends BaseServiceImpl{
+    public void demo(){
+    }
+    public static void main(String[] args){
+
+    }
+}
